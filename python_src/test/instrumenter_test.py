fe278d08c97b3e771dad484eb7594b4c61f00bf4
Theodoros Theodoridis <theodoros.theodoridis@inf.ethz.ch>
2022-12-09 12:35:42 +0100

    [instrumenter] sanity checks, tests and simplifications

--- python_src/test/instrumenter_test.py
+++ python_src/test/instrumenter_test.py
@@ -24,7 +24,7 @@ def get_system_gcc_O0() -> CompilationSetting:
     )
 
 
-def test_instr() -> None:
+def test_instrumentation() -> None:
     iprogram = instrument_program(
         SourceProgram(
             code="""
@@ -50,17 +50,63 @@ def test_instr() -> None:
     gcc = get_system_gcc_O0()
     assert set(("DCEMarker0_", "DCEMarker1_")) == set(iprogram.find_alive_markers(gcc))
     assert set() == set(iprogram.find_dead_markers(gcc))
+
+
+def test_disable_markers() -> None:
+    iprogram = instrument_program(
+        SourceProgram(
+            code="""
+    int foo(int a){
+        if (a)
+            return 1;
+        return 0;
+    }
+    """,
+            language=Language.C,
+        ),
+    )
+
+    gcc = get_system_gcc_O0()
+
     with pytest.raises(AssertionError):
         iprogram.disable_markers(("DCEMarker2_",))
+
     iprogram0 = iprogram.disable_markers(("DCEMarker1_",))
+    assert set(("DCEMarker1_",)) == set(iprogram0.disabled_markers)
     assert set(("DCEMarker0_",)) == set(iprogram0.find_alive_markers(gcc))
     assert set(("DCEMarker1_",)) == set(iprogram0.find_dead_markers(gcc))
+    # disabling the same marker twice shouldn't have any effect
+    assert iprogram0 == iprogram0.disable_markers(("DCEMarker1_",))
 
-    iprogram1 = iprogram.make_markers_unreachable(("DCEMarker0_",))
+    iprogram1 = iprogram.disable_markers(("DCEMarker0_",))
+    assert set(("DCEMarker0_",)) == set(iprogram1.disabled_markers)
     assert set(("DCEMarker1_",)) == set(iprogram1.find_alive_markers(gcc))
     assert set(("DCEMarker0_",)) == set(iprogram1.find_dead_markers(gcc))
 
-    iprogram2 = iprogram.disable_all_markers()
+    iprogram1_1 = iprogram1.disable_remaining_markers()
+    assert set(
+        (
+            "DCEMarker0_",
+            "DCEMarker1_",
+        )
+    ) == set(iprogram1_1.disabled_markers)
+    assert set() == set(iprogram1_1.find_alive_markers(gcc))
+    assert set(
+        (
+            "DCEMarker0_",
+            "DCEMarker1_",
+        )
+    ) == set(iprogram1_1.find_dead_markers(gcc))
+
+    iprogram2 = iprogram.disable_remaining_markers()
+    # disabling all remaining markers twice shouldn't have any effect
+    assert iprogram2 == iprogram2.disable_remaining_markers()
+    assert set(
+        (
+            "DCEMarker0_",
+            "DCEMarker1_",
+        )
+    ) == set(iprogram2.disabled_markers)
     assert set(()) == set(iprogram2.find_alive_markers(gcc))
     assert set(("DCEMarker0_", "DCEMarker1_")) == set(iprogram2.find_dead_markers(gcc))
 
@@ -81,11 +127,54 @@ def test_unreachable() -> None:
             language=Language.C,
         ),
     )
-    iprogram = iprogram.make_markers_unreachable(("DCEMarker1_",))
     gcc = get_system_gcc_O0()
+
+    with pytest.raises(AssertionError):
+        iprogram.make_markers_unreachable(("DCEMarker2_",))
+
+    iprogram = iprogram.make_markers_unreachable(("DCEMarker1_",))
+    # Making the same marker unreachable twice shouldn't have any effect
+    iprogram == iprogram.make_markers_unreachable(("DCEMarker1_",))
+    assert set(("DCEMarker1_",)) == set(iprogram.unreachable_markers)
+
     asm = gcc.get_asm_from_program(iprogram)
     assert "bar" not in asm
 
 
-if __name__ == "__main__":
-    test_instr()
+def test_disable_and_unreachable() -> None:
+    iprogram = instrument_program(
+        SourceProgram(
+            code="""
+    int bar();
+    int foo(int a){
+        if (a){
+            bar();
+            return 1;
+        }
+        return 0;
+    }
+    """,
+            language=Language.C,
+        ),
+    )
+    gcc = get_system_gcc_O0()
+
+    iprogram = iprogram.make_markers_unreachable(("DCEMarker1_",))
+    with pytest.raises(AssertionError):
+        # We can't disable a marker that was already make unreachable
+        iprogram.disable_markers(("DCEMarker1_",))
+    iprogram = iprogram.disable_markers(("DCEMarker0_",))
+    with pytest.raises(AssertionError):
+        # We can't make ureachable a marker that was already disabled
+        iprogram.make_markers_unreachable(("DCEMarker0_",))
+    assert set(("DCEMarker1_",)) == set(iprogram.unreachable_markers)
+    assert set(("DCEMarker0_",)) == set(iprogram.disabled_markers)
+
+    assert set(()) == set(iprogram.find_alive_markers(gcc))
+    assert set(("DCEMarker0_", "DCEMarker1_")) == set(iprogram.find_dead_markers(gcc))
+
+    asm = gcc.get_asm_from_program(iprogram)
+    assert "bar" not in asm
+
+    # All markers have already been disabled or made unreachable
+    assert iprogram == iprogram.disable_remaining_markers()
