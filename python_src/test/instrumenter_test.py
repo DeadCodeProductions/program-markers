469573afd30b4cb1253fffb2f797266a936df5e5
Theodoros Theodoridis <theodoros.theodoridis@inf.ethz.ch>
2022-12-09 10:17:03 +0100

    Add test for unreachable

--- python_src/test/instrumenter_test.py
+++ python_src/test/instrumenter_test.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
 import pytest
-from ccbuilder import CompilerProject
 from diopter.compiler import (
     CompilationSetting,
     CompilerExe,
+    CompilerProject,
     Language,
     OptLevel,
     SourceProgram,
@@ -63,6 +63,28 @@ def test_instr() -> None:
     iprogram2 = iprogram.disable_all_markers()
     assert set(()) == set(iprogram2.find_alive_markers(gcc))
     assert set(("DCEMarker0_", "DCEMarker1_")) == set(iprogram2.find_dead_markers(gcc))
+
+
+def test_unreachable() -> None:
+    iprogram = instrument_program(
+        SourceProgram(
+            code="""
+    int bar();
+    int foo(int a){
+        if (a){
+            bar();
+            return 1;
+        }
+        return 0;
+    }
+    """,
+            language=Language.C,
+        ),
+    )
+    iprogram = iprogram.make_markers_unreachable(("DCEMarker1_",))
+    gcc = get_system_gcc_O0()
+    asm = gcc.get_asm_from_program(iprogram)
+    assert "bar" not in asm
 
 
 if __name__ == "__main__":
