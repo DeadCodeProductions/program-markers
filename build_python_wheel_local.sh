84cb3c22074d73d2ce9d264b4af88d3eda8c24fc
Theodoros Theodoridis <theodoros.theodoridis@inf.ethz.ch>
2023-01-12 16:42:05 +0100

    Fix CI

--- build_python_wheel_local.sh
+++ build_python_wheel_local.sh
@@ -8,7 +8,9 @@ cd build
 cmake -G Ninja .. -DCMAKE_BUILD_TYPE=Release \
                   -DCMAKE_CXX_FLAGS="-static-libgcc -static-libstdc++ -fPIC" \
                   -DCMAKE_EXE_LINKER_FLAGS="-static-libgcc -static-libstdc++" \
-                  -DCMAKE_SHARED_LINKER_FLAGS="-static-libgcc -static-libstdc++"
+                  -DCMAKE_SHARED_LINKER_FLAGS="-static-libgcc -static-libstdc++" \
+                  -DCMAKE_MODULE_PATH="$CMAKE_MODULE_PATH"
+                  
 ninja 
 cd ..
 cp build/bin/dead-instrument python_src/dead_instrumenter
