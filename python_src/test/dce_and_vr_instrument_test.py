bbf3cf89ef8e61c536277074b340ab9fbb6f4f9a
Theodoros Theodoridis <theodoros.theodoridis@inf.ethz.ch>
2023-05-17 18:07:16 +0200

    Add option to instrument with both DCE and VR markers

---
+++ python_src/test/dce_and_vr_instrument_test.py
@@ -0,0 +1,41 @@
+from diopter.compiler import Language, SourceProgram
+from program_markers.instrumenter import InstrumenterMode, instrument_program
+from program_markers.markers import DCEMarker, VRMarker
+
+from .utils import get_system_gcc_O0
+
+
+def test_instrumentation() -> None:
+    iprogram = instrument_program(
+        SourceProgram(
+            code="""
+    int foo(int a){
+        if (a)
+            return 1;
+        return 0;
+    }
+    """,
+            language=Language.C,
+        ),
+        mode=InstrumenterMode.DCE_AND_VR,
+    )
+
+    all_markers = set(
+        (
+            VRMarker.from_str("VRMarkerLE0_"),
+            VRMarker.from_str("VRMarkerGE1_"),
+            DCEMarker.from_str("DCEMarker2_"),
+            DCEMarker.from_str("DCEMarker3_"),
+        )
+    )
+
+    assert all_markers == set(iprogram.enabled_markers)
+
+    gcc = get_system_gcc_O0()
+    assert all_markers == set(iprogram.find_non_eliminated_markers(gcc))
+    assert set() == set(iprogram.find_eliminated_markers(gcc))
+
+    eliminated_markers = iprogram.find_eliminated_markers(gcc)
+    non_eliminated_markers = iprogram.find_non_eliminated_markers(gcc)
+    assert set() == set(eliminated_markers)
+    assert all_markers == set(non_eliminated_markers)
